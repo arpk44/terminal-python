20fdd3baf4e00093f0ebf5fa06704d56253498fd
Arpit Kesharwani <62772567+arpk44@users.noreply.github.com>
2020-08-04 17:34:45 -0400

    Add files via upload

---
+++ pythonterminal.py
@@ -0,0 +1,15 @@
+print("Welcome to Python Terminal")
+print("Press any key to start")
+input(":")
+#Downloads
+
+directories = ("Home", "Downloads", "Pictures", "Terminal Production")
+# Configuration
+
+print("owner.jfjf.pc-mac#4 is your device number")
+
+while True:
+    statement = input("char-meta:").lower()
+    if statement == "ls":
+        print(directories)
+    els
